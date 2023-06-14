a6e9c6e083530c6224b92ab515934b996b65d586
uma-teli <umateli519@gmail.com>
2023-06-14 12:59:18 -0700

    commented

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class AccountList(tornado.web.RequestHandler):
         
         account=int(self.get_argument('account'))
         print(account)
-        #result=client.sql.execute(f"SELECT * FROM TCS001_ACCOUNT WHERE Account_Number= {account} ").result()
+       
         result1=client.sql.execute(f"SELECT * FROM TCS001_TRANSACTION WHERE Account_Number= {account} ").result()
         
 
@@ -96,7 +96,7 @@ class AccountList(tornado.web.RequestHandler):
 
         
        
-        client.shutdown()
+        
        
        
 
@@ -113,14 +113,6 @@ class AccountTransaction(tornado.web.RequestHandler):
         
         act=int(self.get_argument("act"))
         print(act)
-        #damt=int(self.get_argument("damt"))
-        #print(damt)
-
-        
-       
-       
-
-       
         result1=client.sql.execute(f"SELECT * FROM TCS001_TRANSACTION WHERE Account_Number= {act}").result()
         for row in result1:
             print(row.get_object("Available_Balance"))
@@ -133,14 +125,6 @@ class AccountTransaction(tornado.web.RequestHandler):
             transamt=row["Trans_Amount"]
             bankid=row["Bank_Id"]
             desc=row["Description"]
-
-
-        #balance1=balance+damt
-        #client.sql.execute(f"UPDATE TCS001_TRANSACTION SET Available_Balance= {balance1} WHERE Account_Number={act}").result()
-        #result3=client.sql.execute(f"SELECT * FROM TCS001_TRANSACTION WHERE Account_Number= {act}").result()
-        #for row in result3:
-            #print(row.get_object("Available_Balance"))
-            #b=row["Available_Balance"]
             
 
         self.render("static/AccountTransaction.html",Accountno=Account_no,balance=balance,tamt=transamt,bid=bankid,desc=desc,bloc="AccountTransaction")
@@ -153,67 +137,12 @@ class AccountTransaction(tornado.web.RequestHandler):
 
         
        
-class AccountDetails(tornado.web.RequestHandler):
-    def post(self):
-        logging.basicConfig(level=logging.INFO)
-        client = hazelcast.HazelcastClient(
-            cluster_name="zdih-tcs",
-            cluster_members=[
 
-                "192.86.32.113:6701",
-                ])
         
-        acct=int(self.get_argument("acct"))
-        print(acct)
-        camt=int(self.get_argument("camt"))
-        print(camt)
-       
-        result1=client.sql.execute(f"SELECT * FROM TCS001_TRANSACTION WHERE Account_Number= {acct}").result()
-        for row in result1:
-            print(row.get_object("Available_Balance"))
-            print(row.get_object("Account_Number"))
-            Account_no=row["Account_Number"]
-            balance=row["Available_Balance"]
-
-
-
-        balance2=balance-camt
-        client.sql.execute(f"UPDATE TCS001_TRANSACTION SET Available_Balance= {balance2} WHERE Account_Number={acct}").result()
-        result4=client.sql.execute(f"SELECT * FROM TCS001_TRANSACTION WHERE Account_Number= {acct}").result()
-        for row in result4:
-            print(row.get_object("Available_Balance"))
-            b=row["Available_Balance"]
-
-        self.render("static/AccountDetails.html",accountno=Account_no,balance=b,bloc="AccountDetails")
-       
-            
-
-        #base_url='http//192.867.32.113:16099/zdih/rest/api/v1/accounts='
-        #account=str(self.get_body_argument('account'))
-        #headers = {'Content-Type': 'application/json'}
-        #end_url= base_url+str(self.get_body_argument("account"))
-        #req = requests.get(end_url, headers=headers, auth=('ibmuser', 'tcs2041'), verify=False)
-        #json_out = req.json()
-       # print("json")
-        #print(json_out)
-        #self.render("static/AccountDetails.html",accountno= json_out[0][0], balance=json_out[0][1] ,ID= json_out[0][2],bloc="AccountDetails") 
-
-
-        ###cursor=conn.cursor()
-        ##records= cursor.fetchall()
-
-        #cursor.close()
-        #conn.close()
-
-        #self.render("static/AccountDetails.html",accountno=records[0][0], balance=records[0][1] ,ID=records[0][2],bloc="AccountDetails") 
-
-
-
 if __name__ == "__main__":
     app = tornado.web.Application([
         (r"/", landingPage),
         (r"/AccountList", AccountList),
-        (r"/AccountDetails",AccountDetails),
         (r"/AccountTransaction",AccountTransaction)
         
 
